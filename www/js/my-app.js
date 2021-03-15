568574f88704aeb557dd106e5b2cfbff911a081b
drvaderf <franconob112011@gmail.com>
2021-03-15 19:35:36 -0300

    mapa, desplazamiento, UI, bubbleMarker

--- www/js/my-app.js
+++ www/js/my-app.js
@@ -11,7 +11,7 @@ var app = new Framework7({
     id: 'com.negociapp.test',
     // Enable swipe panel
     panel: {
-      swipe: 'left',
+      swipe: false,
     },
     // Add default routes
     routes: [
@@ -22,6 +22,9 @@ var app = new Framework7({
       {
         path: '/register/',
         url: 'register.html',
+        options: {
+        transition: 'f7-circle',
+        }
       },
       {
         path: '/index/',
@@ -36,6 +39,10 @@ var mainView = app.views.create('.view-main');
 var db = firebase.firestore();
 var userCol = db.collection("usuarios");
 
+var map, platform;
+var pos, latitud, longitud;
+//var mapEvents, behavior;
+
 
 
 // Handle Cordova Device Ready Event
@@ -44,6 +51,11 @@ $$(document).on('deviceready', function() {
 
     $$('#comp').on('click', sesionCheck);
     $$('#signOut').on('click', logOut);
+
+    platform = new H.service.Platform({
+        'apikey': 'gY5i9nh39CW9Hkb8itf7umEsECDyQzdTFVq9Oy5dEiU'
+    });
+
 
 });
 
@@ -67,8 +79,47 @@ $$(document).on('page:init', '.page[data-name="search"]', function (e) {
         console.log('error '+error);
     })
 
-    /*$$('#comp').on('click', sesionCheck);
-    $$('#signOut').on('click', logOut);*/
+    var icon = new H.map.Icon('img/storeicon.png');
+    var defaultLayers = platform.createDefaultLayers();
+
+    // observador de posición
+    // var watchID = navigator.geolocation.watchPosition(onSuccess, onError, { timeout: 30000 });
+
+    // Instantiate (and display) a map object:
+    map = new H.Map(document.getElementById('mapContainer'),
+        defaultLayers.vector.normal.map,
+        {
+        zoom: 14,
+        center: { lat: latitud, lng: longitud }
+        });
+        
+        coords = {lat: latitud, lng: longitud};
+        marker = new H.map.Marker(coords, {icon: icon});
+        // Add the marker to the map and center the map at the location of the marker:
+        map.addObject(marker);
+        map.setCenter(coords);
+
+        //desplazamiento
+        behavior = new H.mapevents.Behavior(new H.mapevents.MapEvents(map));
+
+        //interfaz
+        ui = H.ui.UI.createDefault(map, defaultLayers, 'es-ES');
+        ui.getControl('mapsettings').setDisabled(true);
+
+        //burbuja al tocar el marcador
+        map.addEventListener('tap', function(t){
+            if (t.target == marker){
+                console.log('tap tap');
+                
+                var bubble = new H.ui.InfoBubble({ lng: longitud, lat: latitud }, {
+                    content: '<b>Hello World!</b>'
+                });
+                // agregar la burbuja al mapa
+                ui.addBubble(bubble);
+            }
+
+        });
+
 
 })
 
@@ -83,6 +134,30 @@ $$(document).on('page:init', '.page[data-name="index"]', function (e) {
         mainView.router.navigate('/register/');
     })
 
+    onSuccess = function(position) {
+        latitud = position.coords.latitude;
+        longitud = position.coords.longitude;
+        console.log(latitud);
+        console.log(longitud);
+        /*alert('Latitude: '          + position.coords.latitude          + '\n' +
+              'Longitude: '         + position.coords.longitude         + '\n' +
+              'Altitude: '          + position.coords.altitude          + '\n' +
+              'Accuracy: '          + position.coords.accuracy          + '\n' +
+              'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
+              'Heading: '           + position.coords.heading           + '\n' +
+              'Speed: '             + position.coords.speed             + '\n' +
+              'Timestamp: '         + position.timestamp                + '\n');*/
+    };
+ 
+    // onError Callback receives a PositionError object
+    //
+    onError = function(error) {
+        alert('code: '    + error.code    + '\n' +
+              'message: ' + error.message + '\n');
+    }
+ 
+    navigator.geolocation.getCurrentPosition(onSuccess, onError);
+    
 })
 
 
